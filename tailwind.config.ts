a672965a67a746e70c2d8d081c0cbb6309511602
shimeming <shimeming@gmail.com>
2024-12-28 19:37:40 +0800

    Set up project configuration and initial components
    - Enforce semicolons in eslint.config.mjs
    - Remove default page content
    - Add basic skeleton for NavBar.tsx
    - Add font families and primary colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from "tailwindcss";
+const colors = require('tailwindcss/colors')
 
 export default {
   content: [
@@ -9,8 +10,15 @@ export default {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        dark: "#171717", // neutral-900
+        light: "#f5f5f5", // neutral-100
+        primary: "#1d4ed8", // blue-700
+        primaryDark: "#a78bfa" // violet-400
+      },
+      fontFamily: {
+        sans: ['var(--font-noto-sans)'],
+        sanstc: ['var(--font-noto-sans-tc)'],
+        mono: ['var(--font-geist-mono)'],
       },
     },
   },
