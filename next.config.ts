997ec6fa8632efd8caf3ba35dbaf11eb2242492f
shimeming <shimeming@gmail.com>
2024-12-29 23:04:56 +0800

    Update Next.js configuration for static exports

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  basePath: '', // since the repo is shimeming.github.io, I don't need a base path
+  output: 'export', // enable static exports
 };
 
 export default nextConfig;
